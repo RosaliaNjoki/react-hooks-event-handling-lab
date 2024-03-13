d6277a780bd0040183305071234d500d886c4f89
RosaliaNjoki <njosali0@gmail.com>
2024-03-14 00:26:37 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+function EyesOnME (){
+    function handleFocus(){
+        console.log("Good!")
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button value ="Eyes on me" onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+
+}
+ export default EyesOnME
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
-
 function Keypad (){
+    function handleChange(){
+        console.log('Entering password...')
+
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name="password" onChange={handleChange}></input>
+        </div>
     )
 }
 
